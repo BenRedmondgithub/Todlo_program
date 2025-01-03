5bc74b648c5d6ccacd2752c764f7bb0ec2ac5958
Ben Redmond <73973708+BenRedmondgithub@users.noreply.github.com>
2025-01-03 23:24:27 +0000

    TODO program

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+var userChoice = Console.ReadLine;
+
+switch (userChoice) {
+
+    case "E":
+    case "e":
+    Console.WriteLine("Exit");
+    break;
+
+    case "S":
+    case "s":
+    Console.WriteLine("See all TODOs");
+    break;
+
+    case "A":
+    case "a":
+    Console.WriteLine("Add a TODO");
+    break;
+
+    case "R":
+    case "r":
+    Console.WriteLine("Remove a TODO");
+    break;
+
+    default:
+    Console.WriteLine("Invalid choice");
+}
+
+
+Console.ReadKey();
+

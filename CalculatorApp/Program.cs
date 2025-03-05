fc4b4605ae55f2ed8bc52e5eff5242ebc5c61938
Ben Redmond <73973708+BenRedmondgithub@users.noreply.github.com>
2025-03-05 10:17:37 +0000

    Updated files

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("Input the first number:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(""+number1);
+
+
+Console.WriteLine("Input the second number:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(""+number2);
